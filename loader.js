8157ba9bb6e48da7750965bac110a4fbde8a4fc1
Bartosz Kowalczyk <bartoszkowalczyk@wp.pl>
2022-12-22 14:22:20 +0100

    script order change

--- loader.js
+++ loader.js
@@ -3,7 +3,7 @@ function changeVisibility() {
         document.getElementById('container').classList.add('visible');
         document.getElementById('loader').classList.remove('visible');
         document.getElementById('circle').classList.remove('visible');
-    }, 2000);
+    }, 3000);
 
 };
 
