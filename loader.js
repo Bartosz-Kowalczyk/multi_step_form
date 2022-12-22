b2359496e69bb3d2649c93a7d503202494ff1129
Bartosz Kowalczyk <bartoszkowalczyk@wp.pl>
2022-12-22 13:17:28 +0100

    loading time fix

--- loader.js
+++ loader.js
@@ -3,7 +3,7 @@ function changeVisibility() {
         document.getElementById('container').classList.add('active');
         document.getElementById('loader').classList.remove('active');
         document.getElementById('circle').classList.remove('active');
-    }, 1000);
+    }, 2000);
 
 };
 
